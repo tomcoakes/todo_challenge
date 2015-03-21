8ae8202456ac6ac13155f52e152ad4687e63fe3e
Tom Coakes <tomcoakes@gmail.com>
2015-03-21 16:50:47 +0000

    Created new protractor feature, yet to write the tests

--- tests/spec.js
+++ tests/spec.js
@@ -1,8 +1,17 @@
 describe("ToDo App", function() {
 
-  it("should have a title", function() {
-    browser.get("http://localhost:3000");
-    expect(browser.getTitle()).toEqual("Tom's ToDos");
+  describe("Test", function() {
+    it("should have a title", function() {
+      browser.get("http://localhost:3000");
+      expect(browser.getTitle()).toEqual("Tom's ToDos");
+    });
   });
+
+  describe("Creating a task", function() {
+    it("can add a task and display it", function() {
+      browser.get("http://localhost:3000");
+    });
+  });
+
 
 });
