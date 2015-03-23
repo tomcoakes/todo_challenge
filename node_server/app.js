2e1c691e49b1092b423f8e0ab6018a5be70dc571
Tom Coakes <tomcoakes@gmail.com>
2015-03-23 16:00:19 +0000

    Heroku should now be able to set the port

--- node_server/app.js
+++ node_server/app.js
@@ -10,6 +10,8 @@ var users = require('./routes/users');
 
 var app = express();
 
+var port = process.env.PORT || 3000;
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
